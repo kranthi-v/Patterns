a97d04b185e01b86851f73515bbaa6eb1a553204
kranthi-v <krkranthikumar2003@gmail.com>
2025-10-07 12:24:29 +0530

    pattern

---
+++ diamond.py
@@ -0,0 +1,20 @@
+n= int(input("enter no of rows: "))
+spaces = n//2
+stars = 1
+
+for row in range(1,n+1):
+
+    for sp in range(1,spaces+1):
+        print(' ',end='')
+    for st in range(1,stars+1):
+        print('*',end='')
+    print()
+    if row <= n//2:
+        spaces -= 1
+        stars += 2
+    else:
+        spaces += 1
+        stars -= 2
+
+
+    
