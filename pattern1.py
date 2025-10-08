35fcc825b058215ed1575d5d6d4e92ef3d270c84
kranthi-v <krkranthikumar2003@gmail.com>
2025-10-08 12:00:07 +0530

    pattern

---
+++ pattern1.py
@@ -0,0 +1,13 @@
+n = int(input("enter no of rows: "))
+stars = 1
+
+for row in range(1, n+1):
+
+    for st in range(1, stars+1):
+        print('*',end='')
+    print()
+
+    if row <= n//2:
+        stars += 2
+    else:
+        stars -= 2
