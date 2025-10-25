0bdf336685114cc0230a22d2ad525db3e2b9dcf8
kranthi-v <krkranthikumar2003@gmail.com>
2025-10-25 21:40:53 +0530

    number

---
+++ number12.py
@@ -0,0 +1,20 @@
+n = int(input('Enter no of rows: '))
+spaces = n//2
+number = 1
+for row in range(1, n+1):
+    dummy = 1
+    for sp in range(1, spaces+1):
+        print(' ', end='')
+    for num in range(1, number+1):
+        print(dummy, end='')
+        if num <= number//2:
+            dummy += 1
+        else:
+            dummy -= 1
+    print()
+    if row <= n//2:
+        spaces -= 1
+        number += 2
+    else:
+        spaces += 1
+        number -= 2
